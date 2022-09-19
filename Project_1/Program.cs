f65517f56a6eaa0c0e388df7cc466a027e714125
Oleg_bul <kokseg63@gmail.com>
2022-09-19 23:01:33 +0400

    project_1 added

--- Project_1/Program.cs
+++ Project_1/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿//void int string bool
+void WriteTwo()
+{
+    Console.WriteLine(2);
+}
+void WriteHello(string name)
+{
+    Console.WriteLine("Hello, "+ name);
+}
+string HelloToWord(string name)
+{
+    return "Hello, " + name;
+}
+WriteTwo();
+string word = HelloToWord("Oleg");
+Console.WriteLine(word + " Как дела ?");
